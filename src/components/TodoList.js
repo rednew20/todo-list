36ca18e1f4e5464ecfc58394b607a178d1aad562
rednew20 <rednew20@gmail.com>
2022-02-22 19:01:24 -0600

    first commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TodoList = () => {
+  return (
+    <div>
+      <div>
+        <h1>Task List</h1>
+        <input type="text" name="todolstname" />
+        <button className='btn'>Create List</button>
+      </div>
+    </div>
+  )
+}
+
+export default TodoList
